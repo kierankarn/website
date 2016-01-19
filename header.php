e662400291605f6d3a4aabc9fb7427712e39bea7
kierankarn <kieran@tomango.co.uk>
2016-01-19 08:23:06 +0000

    Added all files that are so far on the website

---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/style.css" media="screen">
+	<title>Kieran Karn</title>
+</head>
+<body>
+	<div class="overlay">	
+		<nav>
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="about.php">About</a></li>
+				<li><a href="portfolio.php">Portfolio</a></li>
+				<li><a href="blog.php">Blog</a></li>
+			</ul>
+		</nav>
+	</div>
+	<header class="header">
+		<a href="index.php"><img src="assets/images/logo.png" alt="logo"></a>
+		<button id="overlay-menu" class="menuButton" type="button"><i class="fa fa-bars"></i></button>
+	</header>
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script>
+	$( "#overlay-menu" ).on('click', function() {
+		$( ".overlay" ).toggleClass('overlay-open');
+		$( this ).find('.fa').toggleClass('fa-bars fa-times-circle');
+	});	
+
+</script>
+</body>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include 'header.php';?>
+
+<div class="main" id="main">
+	<div class="header-image">
+	</div>
+</div>
+
+<?php include 'footer.php'; ?>
