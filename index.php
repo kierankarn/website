bf325083d64dcb926be4d037605a5c7af761aa73
kierankarn <kieran@tomango.co.uk>
2016-02-04 08:22:04 +0000

    Added columns to content area

--- index.php
+++ index.php
@@ -1,31 +1,19 @@
 <?php include 'header.php';?>
 
 <div class="main" id="main">
-	<div class="wrap">
-	<div role="main" class="content">
-	<h1>Home</h1>
-	<p>Lorem ipsum dolor sit amet, id alia lobortis mel, cum te epicuri accusata aliquando, 
-	ludus sadipscing cu per. Duo philosophia mediocritatem et, eu zril doming omittantur mel. 
-	Ex bonorum mentitum neglegentur sea, pro altera aliquip moderatius ea, pro an adipisci senserit. 
-	Has cu cibo nobis, an quod expetenda scripserit sed, mel te idque aeterno saperet. Modus invidunt ut eam.
-	<br/><br/>
-	Lorem ipsum dolor sit amet, id alia lobortis mel, cum te epicuri accusata aliquando, 
-	ludus sadipscing cu per. Duo philosophia mediocritatem et, eu zril doming omittantur mel. 
-	Ex bonorum mentitum neglegentur sea, pro altera aliquip moderatius ea, pro an adipisci senserit. 
-	Has cu cibo nobis, an quod expetenda scripserit sed, mel te idque aeterno saperet. Modus invidunt ut eam.
-	<br/><br/>
-	Lorem ipsum dolor sit amet, id alia lobortis mel, cum te epicuri accusata aliquando, 
-	ludus sadipscing cu per. Duo philosophia mediocritatem et, eu zril doming omittantur mel. 
-	Ex bonorum mentitum neglegentur sea, pro altera aliquip moderatius ea, pro an adipisci senserit. 
-	Has cu cibo nobis, an quod expetenda scripserit sed, mel te idque aeterno saperet. Modus invidunt ut eam.
-	<br/><br/>
-	Lorem ipsum dolor sit amet, id alia lobortis mel, cum te epicuri accusata aliquando, 
-	ludus sadipscing cu per. Duo philosophia mediocritatem et, eu zril doming omittantur mel. 
-	Ex bonorum mentitum neglegentur sea, pro altera aliquip moderatius ea, pro an adipisci senserit. 
-	Has cu cibo nobis, an quod expetenda scripserit sed, mel te idque aeterno saperet. Modus invidunt ut eam.</p>
-	</div>
-
-	</div>
+		<div class="grid">
+			<div class="row">
+				<div class="col-10">
+					<h1>Home</h1>
+					<p>Lorem ipsum dolor sit amet, ei omnes soluta qui, ius nonumy semper cu. Duis exerci vel ut, at postea diceret sapientem qui, quo ea legimus placerat quaerendum. An utinam iuvaret mel, quidam verterem vim in, alii legendos ne sea. Te duis necessitatibus duo, perfecto salutandi erroribus eum no. Eam at amet tation accumsan, pri tota dicunt accumsan ea.
+					Cu quo tota vitae, mei ex noluisse sapientem, reque offendit ea quo. Tale expetenda signiferumque et vix. Mei at nisl dicunt, ei munere tempor pro. An vim exerci discere voluptatum.
+					Pro sanctus meliore id, ut duo saperet omittantur interpretaris. Cu his viris deserunt nominati, an eros facilisis sit. Quo dolore detracto an. Duo ut ancillae nominati, iusto impedit omittantur ex eum. Cu reque regione dolorem eum. Sea ut dolore timeam volutpat, qui an facer consul. Unum forensibus id eum, stet adipisci vix eu.
+					Mentitum suscipit singulis et ius, illum mundi dissentiunt mel cu. Ad case vide ius, vis at solum menandri pericula. Eos ut audiam denique blandit, esse dictas id cum, pri ut denique suavitate. Vix ad apeirian consetetur, cum diam tibique hendrerit an. At perfecto pertinacia vis, ridens ocurreret ex quo.
+					An aeque appareat facilisi usu, impetus postulant eam ut. Id mea melius impetus. In prima libris scriptorem est, ne qui alia errem partiendo. Tibique euripidis et per.
+					</p>
+				</div>
+			</div>
+		</div>
 </div>
 
 <?php include 'footer.php'; ?>
